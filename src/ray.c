87c2dc054741a57ed9e1dc2d2e185ddbcaeea796
Christopher Munoz <FunkyCosmonaut@protonmail.com>
2023-04-15 23:46:56 -0800

    pretty colors with normals

--- src/ray.c
+++ src/ray.c
@@ -2,6 +2,7 @@
 #include "vec3.h"
 #include "color.h"
 #include <stdbool.h>
+#include <math.h>
 
 point3 origin(Ray ray){
     return ray.origin;
@@ -15,23 +16,33 @@ vec3 direction(Ray ray){
     return ray.direction;
 }
 
-color ray_color(const Ray ray){
-    if (hit_sphere((point3){0,0,-1}, 0.5, ray)){
-        return (color){1, 0, 0};
-    }
-    vec3 unit_direction = vunit(ray.direction);
-    double t = 0.5*(unit_direction.y + 1.0);
-    return vadd(vmul((color){1.0, 1.0, 1.0},1.0-t), vmul((color){0.5, 0.7, 1.0},t));
-    
+color ray_color(const Ray ray)
+{
+  double t = hit_sphere((point3){0,0,-1}, 0.5, ray);
+  if (t > 0.0){
+    vec3 N = vunit(vsub(at(t, ray), (vec3){0,0,-1}));
+    return vmul((color){N.x + 1, N.y + 1, N.z + 1}, 0.5);
+  }
+  vec3 unit_direction = vunit(ray.direction);
+  t = 0.5*(unit_direction.y + 1.0);
+  return vadd(vmul((color){1.0,1.0,1.0},(1.0 - t)), vmul((color){0.5,0.7,1.0}, t));
 }
 
-bool hit_sphere(const point3 center, double radius, const Ray r){
+bool hit_sphere(const point3 center, double radius, const Ray r)
+{
     vec3 oc = vsub(r.origin, center);
     double a = vdot(r.direction, r.direction);
     double b = 2.0 * vdot(oc, r.direction);
     double c = vdot(oc,oc) - radius*radius;
     double discriminant = (b*b) - (4.0*a*c);
-    return (discriminant > 0);
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return(-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 //note that for discriminants of 0 we can signify an edge.
